d6b9a4cf7f5e5b0dbfe984c6c80ed183d2441a8c
SezarTech <59274933+SezarTech@users.noreply.github.com>
2020-01-26 09:08:36 +0430

    Add files via upload

---
+++ Email.py
@@ -0,0 +1,21 @@
+#SeZaR|@N3Ro0X
+
+
+print ('''
+**************************
+*    Code By @N3Ro0X     *
+*                        *
+*                        *
+*                        *
+*                        *
+**************************
+''')
+a = input ("[!]Enter Your Name to Creat[!]: ")
+b = input ("[!]Enter Your Domains[!]: ")
+c = int(input("Enter Namber: "))
+d = "@"
+i = 0
+while i < c:
+    print (a+str(i)+d+b)
+    i += 1
+print ("Finished")
